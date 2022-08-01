d72409a54662f64e2329864b7679537af58b6975
unkn0wn95 <unkn0wn95@protonmail.com>
2022-08-01 13:00:07 +0800

    Creating Layout and Sections

--- laragigs/resources/views/listing.blade.php
+++ laragigs/resources/views/listing.blade.php
@@ -1,2 +1,13 @@
-<h2><a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a></h2>
-    <p>{{$listing['description']}}</p>
+@extends('layout')
+
+@section('content')
+
+<h2>
+    <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
+</h2>
+    
+<p>
+    {{$listing['description']}}
+</p>
+
+@endsection
--- laragigs/resources/views/listings.blade.php
+++ laragigs/resources/views/listings.blade.php
@@ -1,3 +1,6 @@
+@extends('layout')
+
+@section('content')
 <h1>{{$heading}}</h1>
 
 @unless(count($listings) == 0)
@@ -10,3 +13,5 @@
 @else
 <p>No listings found</p>
 @endunless
+
+@endsection
