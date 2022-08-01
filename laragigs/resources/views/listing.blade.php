065d76476023e890c77ac6f7690054377f524d76
unkn0wn95 <unkn0wn95@protonmail.com>
2022-08-01 16:23:57 +0800

    Added Template Partials

--- laragigs/resources/views/listing.blade.php
+++ laragigs/resources/views/listing.blade.php
@@ -1,13 +1,12 @@
 @extends('layout')
 
 @section('content')
+    @include('partials._search')
+    <h2>
+        <a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }}</a>
+    </h2>
 
-<h2>
-    <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
-</h2>
-    
-<p>
-    {{$listing['description']}}
-</p>
-
+    <p>
+        {{ $listing['description'] }}
+    </p>
 @endsection
