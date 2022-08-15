02f7fbd80afd34a5564e2bdfa02b5989e3ec7d94
unkn0wn95 <unkn0wn95@protonmail.com>
2022-08-15 23:30:29 +0800

    updated file

--- laragigs/resources/views/listings/index.blade.php
+++ laragigs/resources/views/listings/index.blade.php
@@ -1,18 +1,25 @@
-@extends('layout')
-
-@section('content')
+<x-layout>
+  @if (!Auth::check())
     @include('partials._hero')
-    @include('partials._search')
+  @endif
 
-    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+  @include('partials._search')
 
-        @unless(count($listings) == 0)
-            @foreach ($listings as $listing)
-                <x-listing-card :listing="$listing" />
-            @endforeach
-        @else
-            <p>No listings found</p>
-        @endunless
+  <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
-    </div>
-@endsection
+    @unless(count($listings) == 0)
+
+    @foreach($listings as $listing)
+    <x-listing-card :listing="$listing" />
+    @endforeach
+
+    @else
+    <p>No listings found</p>
+    @endunless
+
+  </div>
+
+  <div class="mt-6 p-4">
+    {{$listings->links()}}
+  </div>
+</x-layout>
